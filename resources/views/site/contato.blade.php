2d15665b85a26388aac60c523f73d0babc7fd68d
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-07 17:05:58 -0300

    feat: add navigation

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,9 @@
 <h3>Contato (View)</h3>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre nós</a></li>
+  </ul>
+</nav>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,9 @@
 <h3>Sobre nós (View)</h3>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre nós</a></li>
+  </ul>
+</nav>
