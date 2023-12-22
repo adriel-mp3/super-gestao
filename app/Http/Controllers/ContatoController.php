c6db7bc5cdea8e94b113d80093f8e3029df5e20a
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-22 12:37:09 -0300

    feat: create salvar to save data with validation

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
 use Illuminate\Http\Request;
 use App\SiteContato;
+
 class ContatoController extends Controller
 {
    public function contato(Request $request)
    {
-      $contato = new SiteContato();
-      $contato->create($request->all());
+      $motivo_contatos = MotivoContato::all();
+      
+      return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+   }
 
-      return view('site.contato');
+   public function salvar(Request $request)
+   {
+      $request->validate([
+         'nome' => 'required|min:3|max:40',
+         'telefone' =>'required',
+         'email'=> 'required',
+         'motivo_contato' => 'required',
+         'mensagem' => 'required'
+      ]);
+
+      SiteContato::create($request->all());
    }
 }
