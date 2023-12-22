98bbd43e5fdb194e544997bb163cc1c8659d584f
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-22 12:33:53 -0300

    feat: persist data for already populated fields

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,20 +1,25 @@
 <form action="{{ route('site.contato') }}" method="POST">
     {{ $slot }}
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $classe ?? 'borda-branca'}}">
+    <input name="nome" type="text" placeholder="Nome" class="{{ $classe ?? 'borda-branca' }}"
+        value="{{ old('nome') }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe ?? 'borda-branca' }}">
+    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe ?? 'borda-branca' }}"
+        value="{{ old('telefone') }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $classe ?? 'borda-branca' }}">
+    <input name="email" type="text" placeholder="E-mail" class="{{ $classe ?? 'borda-branca' }}"
+        value="{{ old('email') }}">
     <br>
     <select name="motivo_contato" class="{{ $classe ?? 'borda-branca' }}">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        @foreach ($motivo_contatos as $key => $motivo_contato)
+            <option value="{{ $motivo_contato->id }}"
+                {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>
+                {{ $motivo_contato->motivo_contato }}</option>
+        @endforeach
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $classe ?? 'borda-branca' }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe ?? 'borda-branca' }}">{{ old('mensagem') ?? 'Preencha aqui a sua mensagem' }}</textarea>
     <br>
     <button type="submit" class="{{ $classe ?? 'borda-branca' }}">ENVIAR</button>
 </form>
