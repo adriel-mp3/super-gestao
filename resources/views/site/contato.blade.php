6748a7d2cf78c73cdc898bb9b23c0e8534c3a454
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-22 12:34:24 -0300

    feat: import motivo contatos to form

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -2,7 +2,7 @@
 
 @section('conteudo')
 
-@section('titulo', 'Contato')
+@section('titulo', $titulo)
 
 <div class="topo">
 
@@ -26,7 +26,7 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+            @component('site.layouts._components.form_contato', ['classe' => 'borda-preta', 'motivo_contatos' => $motivo_contatos])
             <p>A nossa equipe analisará a sua mensagem e retonaremos o mais rápido possível</p>
             <p>Nosso tempo médio de resposta é de 48 hrs</p>
             @endcomponent
