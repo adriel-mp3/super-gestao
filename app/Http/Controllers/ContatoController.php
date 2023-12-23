e90b0a63084a6c10ae989baeb68a31e6890d0dcd
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-12-23 14:30:34 -0300

    feat: enchance form validation

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,14 +17,36 @@ class ContatoController extends Controller
 
    public function salvar(Request $request)
    {
-      $request->validate([
+      $rules = [
          'nome' => 'required|min:3|max:40',
          'telefone' =>'required',
-         'email'=> 'required',
-         'motivo_contato' => 'required',
+         'email'=> 'required|email',
+         'motivo_contato_id' => 'required',
          'mensagem' => 'required'
-      ]);
+      ];
+
+      $message =  [
+         'nome.required' => 'O campo nome precisa ser preenchido',
+         'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+         'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+         'nome.unique' => 'O nome informado já está em uso',
+
+         'telefone.required' => 'O campo telefone precisa ser preenchido',
+
+         'email.required' => 'O campo email precisa ser preenchido',
+         'email.email' => 'O campo email está incorreto',
+
+         'motivo_contato_id.required' => 'O campo motivo contato precisa ser preenchido',
+
+         'mensagem.required' => 'O campo mensagem precisa ser preenchido',
+
+         'required' => 'O campo :attribute deve ser preenchido',
+      ];
+      
+      $request->validate($rules, $message);
 
       SiteContato::create($request->all());
+      
+      return redirect()->route('site.index');
    }
 }
